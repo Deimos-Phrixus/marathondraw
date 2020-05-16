7899c9560722871d5f8a50af3749bf1ea9273eeb
Ridhwanul Haque <rhemon19@gmail.com>
2020-05-16 23:46:44 +0100

    loading canvas when connected

--- frontend/public/server.js
+++ frontend/public/server.js
@@ -9,7 +9,7 @@ socket.onopen = function (e) {
     document.title = "Searching room...";
     // socket.send("BEGIN");
     socket.send("name," + nickname);
-    
+
     console.log("[sent] initial message sent")
     document.title = "Connected";
 
@@ -20,10 +20,12 @@ socket.onmessage = function (event) {
     console.log('[message] Data received from server: ' + event.data);
     switch (event.data.split(",")[0]) {
         case "0":
-            console.log('Player connected and waiting.')
+            console.log('Player connected and waiting.');
+            loadingScreen();
             break;
         case "1":
             console.log('Game started and running.')
+            game();
             break;
     }
 };
