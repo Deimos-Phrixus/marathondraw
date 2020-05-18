14937ca6256e1a7026bffea8fc19073d62950f31
Ridhwanul Haque <rhemon19@gmail.com>
2020-05-18 04:08:18 +0100

    making loading svg more visible

--- frontend/public/paging.js
+++ frontend/public/paging.js
@@ -7,27 +7,28 @@ function loadingScreen() {
     document.getElementById("container").innerHTML = `<!-- LOADING 4 -->
     <div class="loader loader--style4" title="3" id="loadingScreen">
       <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-         width="24px" height="24px" viewBox="0 0 24 24" style="enable-background:new 0 0 50 50;" xml:space="preserve">
-        <rect x="0" y="0" width="4" height="7" fill="#333">
+         width="100px" height="100px" viewBox="0 0 24 24" style="enable-background:new 0 0 50 50;" xml:space="preserve">
+        <rect x="0" y="0" width="4" height="7" fill="#fff">
           <animateTransform  attributeType="xml"
             attributeName="transform" type="scale"
             values="1,1; 1,3; 1,1"
             begin="0s" dur="0.6s" repeatCount="indefinite" />       
         </rect>
 
-        <rect x="10" y="0" width="4" height="7" fill="#333">
+        <rect x="10" y="0" width="4" height="7" fill="#fff">
           <animateTransform  attributeType="xml"
             attributeName="transform" type="scale"
             values="1,1; 1,3; 1,1"
             begin="0.2s" dur="0.6s" repeatCount="indefinite" />       
         </rect>
-        <rect x="20" y="0" width="4" height="7" fill="#333">
+        <rect x="20" y="0" width="4" height="7" fill="#fff">
           <animateTransform  attributeType="xml"
             attributeName="transform" type="scale"
             values="1,1; 1,3; 1,1"
             begin="0.4s" dur="0.6s" repeatCount="indefinite" />       
         </rect>
       </svg>
+      <h3> Finding Players ... </h3>
     </div>`;
 }
 
